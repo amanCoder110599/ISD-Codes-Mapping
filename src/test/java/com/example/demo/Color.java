271718ef75df5a5c4a2ba97dbc01523f78d0d517
Aman Kumar Singh <34673989+amanCoder110599@users.noreply.github.com>
2020-02-06 13:05:08 +0530

    Create Color.java

---
+++ src/test/java/com/example/demo/Color.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+/**
+ * @author Abhimanyu Singh
+ * @author abhimanyusingh@hackerrank.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Color {
+
+    public static final String RESET = "\033[0m";
+
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String WHITE = "\033[0;37m";
+
+    public static final String BLACK_BOLD = "\033[1;30m";
+    public static final String RED_BOLD = "\033[1;31m";
+    public static final String GREEN_BOLD = "\033[1;32m";
+    public static final String YELLOW_BOLD = "\033[1;33m";
+    public static final String BLUE_BOLD = "\033[1;34m";
+    public static final String WHITE_BOLD = "\033[1;37m";
+}
